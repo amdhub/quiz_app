76e29fbaf071187476554da56a962738cf5e19a4
AshDest <ashuzamaheshe4@gmail.com>
2023-09-07 13:27:05 +0200

    first commit

---
+++ lib/datas.dart
@@ -0,0 +1,17 @@
+import 'question.dart';
+
+class Datas{
+
+  List<Question> listeQuestions = [
+    Question(questionText: 'La devise de la Belgique est l\'union fait la force', questionAnswer: true, explication: '', imagePath: 'belgique.JPG'),
+    Question(questionText: 'La lune va finir par tomber sur terre à cause de la gravité', questionAnswer: false, explication: 'Au contraire la lune s\'éloigne', imagePath: 'lune.jpg'),
+    Question(questionText: 'La Russie est plus grande en superficie que Pluton', questionAnswer: true, explication: '', imagePath: 'russie.jpg'),
+    Question(questionText: 'Nyctalope est une race naine d\'antilope', questionAnswer: false, explication: 'C’est une aptitude à voir dans le noir', imagePath: 'nyctalope.jpg'),
+    Question(questionText: 'Le Commodore 64 est l\’oridnateur de bureau le plus vendu', questionAnswer: true, explication: '', imagePath: 'commodore.jpg'),
+    Question(questionText: 'Le nom du drapeau des pirates es black skull', questionAnswer: false, explication: 'Il est appelé Jolly Roger', imagePath: 'pirate.png'),
+    Question(questionText: 'Haddock est le nom du chien Tintin', questionAnswer: false, explication: 'C\'est Milou', imagePath: 'tintin.jpg'),
+    Question(questionText: 'La barbe des pharaons était fausse', questionAnswer: true, explication: 'A l\'époque déjà ils utilisaient des postiches', imagePath: 'pharaon.jpg'),
+    Question(questionText: 'Au Québec tire toi une bûche veut dire viens viens t\'asseoir', questionAnswer: true, explication: 'La bûche, fameuse chaise de bucheron', imagePath: 'buche.jpg'),
+    Question(questionText: 'Le module lunaire Eagle de possédait de 4Ko de Ram', questionAnswer: true, explication: 'Dire que je me plains avec mes 8GO de ram sur mon mac', imagePath: 'eagle.jpg'),
+  ];
+}
---
+++ lib/question.dart
@@ -0,0 +1,13 @@
+class Question{
+  String questionText;
+  bool questionAnswer;
+  String explication;
+  String imagePath;
+
+Question({
+  required this.questionText,
+  required this.questionAnswer,
+  required this.explication,
+  required this.imagePath
+});
+}
