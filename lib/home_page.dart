126c91a368dc2039499cc44e6546cd418b44c945
AshDest <ashuzamaheshe4@gmail.com>
2023-09-07 14:43:47 +0200

    other commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,16 +4,24 @@ import 'package:quiz_app/quiz_page.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    final size = MediaQuery.of(context).size;
+    final height = size.height;
+    final width  = size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
       ),
       body: Center(
         child: Card(
+          color: Colors.deepOrange.shade100,
+          elevation: 10,
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Image.asset('images/cover.jpg'),
+              Padding(padding: EdgeInsets.all(10.0),
+                child: Image.asset('images/cover.jpg', height: height / 2, width: width * 0.8, fit: BoxFit.cover, ),
+              ),
               ElevatedButton(
                   onPressed: (){
                     Navigator.of(context).push(MaterialPageRoute(
